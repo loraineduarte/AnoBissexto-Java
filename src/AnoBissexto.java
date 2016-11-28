ecdb797cfa93fb16b166daff186e483af4edc26a
rafael medeiros <rafaelmedeiros@enacom.com.br>
2016-11-28 15:32:57 -0200

    done

---
+++ src/AnoBissexto.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class AnoBissexto {
+    public static void main (String[] args){
+        Scanner entrada = new Scanner(System.in); //para ler o que o usuário digitar
+
+        System.out.println("Digite o ano:");
+        int ano = entrada.nextInt(); //recebendo o ano
+
+        if((ano%4==0)&&(ano%100!=0)) {
+            System.out.println("É ano bissexto ");
+
+        }
+        else if((ano%4==0)&&(ano%400==0)){
+            System.out.println("É ano bissexto ");
+
+        }
+        else{
+            System.out.println("Não é ano bissexto ");
+
+        }
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
